6bbdf8f97af70dd7db81805e3a06e5fafdaa424f
tsunamibear <thissideofrandom@gmail.com>
2018-02-05 20:32:58 -0600

    SIGSTOP TwitchFix RANDFIX PRECHECK
    Enhancement - Video will freeze on SIGSTOP from XScreensaver now by spinning up second watcher function during XScreensaver only
    Enhancement - Twitch streams will ALWAYS check if stream is online before playing, after video completion for most accurate timing
    Enhancement - Random number is now using a Unsigned 3 Byte value instead of the limitations of $RANDOM. Should be good for videos up to ~194 days now
    Enhancement - Checks for required binaries before trying to execute

--- shuffler.sh
+++ shuffler.sh
@@ -15,20 +15,23 @@
 #~   If a clip is shorter than the duration (Default: 300s)
 #~   The duration will be set to the length of that clip for playback
 #~   This results in the clip being played from start to finish
+#~
+#~   If spun up by XScreensaver, will spawn a watcher function
+#~   This function handles freezing video playback when SIGSTOP
+#~    is sent to base script by XScreensaver for password entry
 
 # VARS
 dur=${1:-300}
 _DIR="${HOME}/.config/shuffler"
-_PPNAME=$(ps -o command= $PPID)
+_PPNAME=$(ps -o comm= $PPID)
 
-# PLAYLIST CHECK
-if [ -f ${_DIR}/playlist ]; then
-   mapfile -t videos < ${_DIR}/playlist && echo "Loaded ${#videos[@]} videos/streams from ${_DIR}/playlist"
-else
-   echo "Couldn't find playlist under ${_DIR}"
-   echo "Create the play list and run again"
+# CLEANUP FUNCTION
+cleanup() {
+   kill -SIGCONT $(jobs -p) 2>/dev/null
+   kill $(jobs -p) 2>/dev/null
    exit
-fi
+}
+trap cleanup INT TERM 
 
 # HELP FUNCTION
 print_help() {
@@ -36,55 +39,16 @@ print_help() {
    sed -ne 's/^#~//p' ${0}
 }
 
-# CLEANUP FUNCTION
-cleanup() {
-   echo "Closing $(jobs -p)"
-   kill $(jobs -p) 2>/dev/null
-   exit
+# RANDOM FUNCTION
+rnum() {
+   # Generate a 3-Byte unsigned int 0-16777215 (~194 Days)
+   od -vAn -N3 -tu < /dev/urandom
 }
-trap cleanup INT TERM
-
-# TEMPORARY SETTING FOR DEMO WINDOW
-if [[ ${_PPNAME} == xscreensaver-demo ]]; then
-   # The xscreensaver demo window passes custom args overwriting duration
-   dur=10
-fi
-
-# PRINT TITLES AND EXIT
-if [[ $@ =~ gettitles ]]; then
-   for i in ${!videos[@]}; do
-
-      # Regular file
-      if [ -f ${videos[$i]} ]; then
-         echo "File name"
-	 echo ${videos[$i]}
-	 echo
-      # Titles for web. Tested with youtube/twitch
-      else
-	 # Grab twitch stream description
-	 if [[ ${videos[$i]} =~ twitch ]]; then
-            echo "Description for ${videos[$i]}"
-	    youtube-dl --get-description ${videos[$i]}
-	 # Youtube video title
-         else
-            echo "Title of ${videos[$i]}"
-	    youtube-dl --get-title ${videos[$i]} 2>/dev/null || echo "Error retrieving..."
-         fi
-
-	 echo
-      fi
-
-   done   
-   exit
-elif [[ ${@} =~ [hH] ]] || [[ ! ${dur} =~ ^[0-9]+$ ]]; then
-   print_help
-   exit
-fi 
 
 # GET TIME IN SECONDS FUNCTION
 gettime () {
    # Start of function
-   echo -n "Getting time(seconds) of ${videos[$1]} - "
+   echo -n "Getting status/time(seconds) of ${videos[$1]} - "
    
    # Get regular file time
    if [ -f ${videos[$1]} ]; then
@@ -130,26 +94,124 @@ playvid () {
    fi
 }
 
+# XSCREENSAVER SIGSTOP && SIGCONT HANDLER
+watcher() {
+   local freeze=0
+
+   # This tries to freeze MPV when the password screen comes up
+   # Normaly XScreensaver passed a SIGSTOP to just the one child
+   # Because this doesn't hit our child and can't be trapped we have to handle this 
+   while :; do
+      # When main script is hit by SIGSTOP from xscreensaver forward to MPV
+      if [[ $freeze -eq 0 ]] && [[ $(ps -q $$ -o state --no-headers) == T ]]; then
+         echo "Freezing $(pgrep -P $$ mpv)"
+	 kill -SIGSTOP $(pgrep -P $$ mpv)
+	 freeze=1
+      # Resume MPV when main pid resumes
+      elif [[ $freeze -eq 1 ]] && [[ ! $(ps -q $$ -o state --no-headers) == T ]]; then
+         echo "Unfreezing $(pgrep -P $$ mpv)"
+	 kill -SIGCONT $(pgrep -P $$ mpv)
+	 freeze=0
+      fi
+      sleep 0.5
+   done
+}
+
+# REQ CHECK
+if ! command -v od > /dev/null; then
+   echo "Missing od command required to run"
+   exit
+elif ! command -v youtube-dl > /dev/null; then
+   echo "Missing youtube-dl command required to run"
+   exit
+elif ! command -v mpv > /dev/null; then
+   echo "Missing mpv command required to run"
+   exit
+fi
+
+# PLAYLIST CHECK
+if [ -f ${_DIR}/playlist ]; then
+   mapfile -t videos < ${_DIR}/playlist && echo "Loaded ${#videos[@]} videos/streams from ${_DIR}/playlist"
+else
+   echo "Couldn't find playlist under ${_DIR}"
+   echo "Create the play list and run again"
+   exit
+fi
+
+# XSCREENSAVER SPECIFICS 
+if [[ ${_PPNAME} == xscreensaver-de ]]; then
+   # The xscreensaver demo window passes custom args overwriting duration
+   dur=10
+elif [[ ${_PPNAME} == xscreensaver ]]; then
+   # Fork this to freeze video playback on password prompt.
+   echo "XScreensaver detected, spinning up SIG watcher for proper freezing"
+   echo
+   watcher &
+fi
+
+# PRINT HELP AND EXIT
+if [[ ${@} =~ [hH] ]] || [[ ! ${dur} =~ ^[0-9]+$ ]]; then
+   print_help
+   exit
+fi 
+
+# PRINT TITLES AND EXIT
+if [[ $@ =~ gettitles ]]; then
+   for i in ${!videos[@]}; do
+
+      # Regular file
+      if [ -f ${videos[$i]} ]; then
+         echo "File name"
+	 echo ${videos[$i]}
+	 echo
+      # Titles for web. Tested with youtube/twitch
+      else
+	 # Grab twitch stream description
+	 if [[ ${videos[$i]} =~ twitch ]]; then
+            echo "Description for ${videos[$i]}"
+	    youtube-dl --get-description ${videos[$i]}
+	 # Youtube video title
+         else
+            echo "Title of ${videos[$i]}"
+	    youtube-dl --get-title ${videos[$i]} 2>/dev/null || echo "Error retrieving..."
+         fi
+	 echo
+      fi
+   done   
+   exit
+fi
+
 # MAIN
 while :; do
    # Generate random index for video to play
-   vindex=$((RANDOM%${#videos[@]}))
+   vindex=$(($(rnum)%${#videos[@]}))
   
+   # Always unset twitch stream times to check if live before this run
+   if [[ ${videos[$vindex]} =~ twitch ]]; then
+      unset -v times[$vindex]
+
+      # Wait sooner for twitch streams to make sure not to check if live too early
+      if pkill -0 -P $$ mpv; then 
+         echo "Waiting to check if ${videos[$vindex]} is LIVE" 
+         wait $(pgrep -P $$ mpv)
+      fi
+   fi
+
    # Get time if we don't have it for next video
    if [ -z "${times[$vindex]}" ]; then gettime $vindex || continue; fi
-
+   
    # Wait for any background videos to finish before playing next
-   wait
-
+   ( pkill -0 -P $$ mpv ) && wait $(pgrep -P $$ mpv)
+   
    # Check if duration is longer than video and adjust accordingly
    if [[ ${times[$vindex]} -lt ${dur} ]]; then
       echo "Adjusting duration for shorter video length - ${times[$vindex]}s" 
       oldur=${dur}
       dur=${times[$vindex]}
-      starttime=$((RANDOM%((${times[$vindex]}+1)-(${dur}))))
+      starttime=$(($(rnum)%((${times[$vindex]}+1)-(${dur}))))
    else
       # Set random start time based on duration
-      starttime=$((RANDOM%((${times[$vindex]}+1)-${dur})))
+      starttime=$(($(rnum)%((${times[$vindex]}+1)-${dur})))
    fi
 
    # Start up video by index. Background so we can get the next video ready to play
@@ -157,7 +219,7 @@ while :; do
    echo "Title: ${titles[$vindex]}"
    echo "Playing ${dur}s starting at ${starttime} out of ${times[$vindex]}s"
    echo
-   playvid ${vindex}; 
+   playvid ${vindex}
 
    # Reset duration if it was changed for a short video
    if [[ ${oldur} -gt 0 ]]; then
