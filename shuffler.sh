39ecf9b99314b9d36b09a1d4e4ec595de463d377
tsunamibear <thissideofrandom@gmail.com>
2018-02-06 22:28:33 -0600

    Enhancements
    Added errcount to handle network runaways and combat CPU spikes
    All variables caps
    Added time logics for youtube-dl return style

--- shuffler.sh
+++ shuffler.sh
@@ -21,9 +21,11 @@
 #~    is sent to base script by XScreensaver for password entry
 
 # VARS
-dur=${1:-300}
+DUR=${1:-300}
+SIZE="--geometry=961x526+959+554"
+ERRCOUNT=0
 _DIR="${HOME}/.config/shuffler"
-_PPNAME=$(ps -o comm= $PPID)
+_PPNAME=$(ps -o comm= ${PPID})
 
 # CLEANUP FUNCTION
 cleanup() {
@@ -48,24 +50,25 @@ rnum() {
 # GET TIME IN SECONDS FUNCTION
 gettime () {
    # Start of function
-   echo -n "Getting status/time(seconds) of ${videos[$1]} - "
+   echo -n "Getting status/time(seconds) of ${VIDEOS[${1}]} - "
    
    # Get regular file time
-   if [ -f ${videos[$1]} ]; then
-      titles[$1]=${videos[$1]##*/}
-      times[$1]=$(date -ud "1970/01/01 $(ffprobe -i ${videos[$1]} -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)" +%s)
+   if [ -f ${VIDEOS[${1}]} ]; then
+      TITLES[${1}]=${VIDEOS[${1}]##*/}
+      TIMES[${1}]=$(date -ud "1970/01/01 $(ffprobe -i ${VIDEOS[${1}]} -show_entries format=duration -v quiet -of csv="p=0" -sexagesimal)" +%s)
    # Catch twitch stream, time will alway equal duration
-   elif [[ ${videos[$1]} =~ twitch ]]; then
-      titles[$1]=$(youtube-dl --get-description ${videos[$1]})
+   elif [[ ${VIDEOS[${1}]} =~ twitch ]]; then
+      TITLES[${1}]=$(youtube-dl --get-description ${VIDEOS[${1}]})
      
       # If stream offline return an error to skip trying to play.
       # This should also allow us to check later if it comes online 
-      if [ $? -gt 0 ]; then
+      if [ ${?} -gt 0 ]; then
+	 ((ERRCOUNT++))
          echo 
          return 1
       # Otherwise we set the time of the video to duration since we will always stream for max time
       else
-         times[$1]=${dur}
+         TIMES[${1}]=${DUR}
 	 echo "LIVE"
 	 echo
        fi
@@ -73,21 +76,26 @@ gettime () {
       return
    # Catch any others, only tested with youtube links
    else
-      mapfile -t results <<<$(youtube-dl --get-title --get-duration ${videos[$1]})
-      titles[$1]=${results[0]}
+      mapfile -t RESULTS <<<$(youtube-dl --get-title --get-duration ${VIDEOS[${1}]})
+      TITLES[${1}]=${RESULTS[0]}
 
       # Format the result returned by --get-duration, adding 00s
-      if [[ ${#results[1]} -eq 5 ]]; then
-         times[$1]=$(date -ud "1970/01/01 00:${results[1]}" +%s)
-      elif [[ ${#results[1]} -eq 2 ]]; then
-         times[$1]=$(date -ud "1970/01/01 00:00:${results[1]}" +%s)
+      if [[ ${#RESULTS[1]} -gt 6 ]]; then
+         TIMES[${1}]=$(date -ud "1970/01/01 ${RESULTS[1]}" +%s)
+      elif [[ ${#RESULTS[1]} -gt 3 ]]; then
+         TIMES[${1}]=$(date -ud "1970/01/01 00:${RESULTS[1]}" +%s)
+      elif [[ ${#RESULTS[1]} -gt 0 ]]; then
+         TIMES[${1}]=$(date -ud "1970/01/01 00:00:${RESULTS[1]}" +%s)
       else
-         times[$1]=$(date -ud "1970/01/01 ${results[1]}" +%s)
+	 ((ERRCOUNT++))
+	 echo
+         unset -v TIMES[${1}]
+	 return 1
       fi
    fi
    
    # Return time for stdout
-   echo "${times[$1]}s" 
+   echo "${TIMES[${1}]}s" 
    echo
 }
 
@@ -95,31 +103,31 @@ gettime () {
 playvid () {
    # Draw to xscreensaver window if run from xscreensaver
    if [[ ${_PPNAME} =~ xscreensaver ]]; then
-      mpv --osc=no --no-stop-screensaver --wid=${XSCREENSAVER_WINDOW} --really-quiet --mute=yes --start=${starttime:-0} --length=${dur} ${videos[$1]} &
+      mpv --network-timeout=5 --osc=no --no-stop-screensaver --wid=${XSCREENSAVER_WINDOW} --really-quiet --mute=yes --start=${STARTTIME:-0} --length=${DUR} ${VIDEOS[${1}]} &
    # Generic play command
    else
-      mpv --osc=no --really-quiet --mute=yes --no-border --geometry=961x526+959+554 --start=${starttime:-0} --length=${dur} ${videos[$1]} &
+      mpv --network-timeout=5 --osc=no --really-quiet --mute=yes --no-border ${SIZE} --start=${STARTTIME:-0} --length=${DUR} ${VIDEOS[${1}]} &
    fi
 }
 
 # XSCREENSAVER SIGSTOP && SIGCONT HANDLER
 watcher() {
-   local freeze=0
+   local FREEZE=0
 
    # This tries to freeze MPV when the password screen comes up
    # Normaly XScreensaver passed a SIGSTOP to just the one child
    # Because this doesn't hit our child and can't be trapped we have to handle this 
    while :; do
       # When main script is hit by SIGSTOP from xscreensaver forward to MPV
-      if [[ $freeze -eq 0 ]] && [[ $(ps -q $$ -o state --no-headers) == T ]] && pkill -0 -P $$ mpv; then
-         echo "Freezing $(pgrep -P $$ mpv)"
-	 kill -SIGSTOP $(pgrep -P $$ mpv)
-	 freeze=1
+      if [[ ${FREEZE} -eq 0 ]] && [[ $(ps -q ${$} -o state --no-headers) == T ]] && pkill -0 -P ${$} mpv; then
+         echo "Freezing $(pgrep -P ${$} mpv)"
+	 kill -SIGSTOP $(pgrep -P ${$} mpv)
+	 FREEZE=1
       # Resume MPV when main pid resumes
-      elif [[ $freeze -eq 1 ]] && [[ ! $(ps -q $$ -o state --no-headers) == T ]] && pkill -0 -P $$ mpv; then
-         echo "Unfreezing $(pgrep -P $$ mpv)"
-	 kill -SIGCONT $(pgrep -P $$ mpv)
-	 freeze=0
+      elif [[ ${FREEZE} -eq 1 ]] && [[ ! $(ps -q ${$} -o state --no-headers) == T ]] && pkill -0 -P ${$} mpv; then
+         echo "Unfreezing $(pgrep -P ${$} mpv)"
+	 kill -SIGCONT $(pgrep -P ${$} mpv)
+	 FREEZE=0
       fi
       sleep 0.5
    done
@@ -139,7 +147,7 @@ fi
 
 # PLAYLIST CHECK
 if [ -f ${_DIR}/playlist ]; then
-   mapfile -t videos < ${_DIR}/playlist && echo "Loaded ${#videos[@]} videos/streams from ${_DIR}/playlist"
+   mapfile -t VIDEOS < ${_DIR}/playlist && echo "Loaded ${#VIDEOS[@]} videos/streams from ${_DIR}/playlist"
 else
    echo "Couldn't find playlist under ${_DIR}"
    echo "Create the play list and run again"
@@ -149,7 +157,7 @@ fi
 # XSCREENSAVER SPECIFICS 
 if [[ ${_PPNAME} == xscreensaver-de ]]; then
    # The xscreensaver demo window passes custom args overwriting duration
-   dur=10
+   DUR=10
 elif [[ ${_PPNAME} == xscreensaver ]]; then
    # Fork this to freeze video playback on password prompt.
    echo "XScreensaver detected, spinning up SIG watcher for proper freezing"
@@ -158,24 +166,24 @@ elif [[ ${_PPNAME} == xscreensaver ]]; then
 fi
 
 # PRINT TITLES AND EXIT
-if [[ $@ =~ gettitles ]]; then
-   for i in ${!videos[@]}; do
+if [[ ${@} =~ gettitles ]]; then
+   for i in ${!VIDEOS[@]}; do
 
       # Regular file
-      if [ -f ${videos[$i]} ]; then
+      if [ -f ${VIDEOS[${i}]} ]; then
          echo "File name"
-	 echo ${videos[$i]}
+	 echo ${VIDEOS[${i}]}
 	 echo
       # Titles for web. Tested with youtube/twitch
       else
 	 # Grab twitch stream description
-	 if [[ ${videos[$i]} =~ twitch ]]; then
-            echo "Description for ${videos[$i]}"
-	    youtube-dl --get-description ${videos[$i]}
+	 if [[ ${VIDEOS[${i}]} =~ twitch ]]; then
+            echo "Description for ${VIDEOS[${i}]}"
+	    youtube-dl --get-description ${VIDEOS[${i}]}
 	 # Youtube video title
          else
-            echo "Title of ${videos[$i]}"
-	    youtube-dl --get-title ${videos[$i]} 2>/dev/null || echo "Error retrieving..."
+            echo "Title of ${VIDEOS[${i}]}"
+	    youtube-dl --get-title ${VIDEOS[${i}]} 2>/dev/null || echo "Error retrieving..."
          fi
 	 echo
       fi
@@ -184,56 +192,80 @@ if [[ $@ =~ gettitles ]]; then
 fi
 
 # PRINT HELP AND EXIT
-if [[ ${@} =~ [hH] ]] || [[ ! ${dur} =~ ^[0-9]+$ ]]; then
+if [[ ${@} =~ [hH] ]] || [[ ! ${DUR} =~ ^[0-9]+$ ]]; then
    print_help
    exit
 fi 
 
 # MAIN
 while :; do
+   # Check error count and sleep if greater than 5
+   if [[ ${ERRCOUNT} -ge 5 ]]; then
+      echo "Error count - ${ERRCOUNT}, sleeping 10s..."
+      echo
+      sleep 10
+
+      # Reset counter
+      ERRCOUNT=0
+   fi
+
    # Generate random index for video to play
-   vindex=$(($(rnum)%${#videos[@]}))
-  
+   VINDEX=$(($(rnum)%${#VIDEOS[@]}))
+ 
    # Always unset twitch stream times to check if live before this run
-   if [[ ${videos[$vindex]} =~ twitch ]]; then
-      unset -v times[$vindex]
+   if [[ ${VIDEOS[${VINDEX}]} =~ twitch ]]; then
+      unset -v TIMES[${VINDEX}]
 
       # Wait sooner for twitch streams to make sure not to check if live too early
-      if pkill -0 -P $$ mpv; then 
-         echo "Waiting to check if ${videos[$vindex]} is LIVE" 
-         wait $(pgrep -P $$ mpv)
+      if pkill -0 -P ${$} mpv; then 
+         echo "Waiting to check if ${VIDEOS[${VINDEX}]} is LIVE" 
+         wait $(pgrep -P ${$} mpv)
+
+	 # Track errors
+	 if [[ ${?} -lt 3 ]]; then
+            ((errcount++))
+         elif [[ ${errcount} -gt 0 ]]; then
+            ((errcount--))
+         fi
       fi
    fi
 
    # Get time if we don't have it for next video
-   if [ -z "${times[$vindex]}" ]; then gettime $vindex || continue; fi
+   if [ -z "${TIMES[${VINDEX}]}" ]; then gettime ${VINDEX} || continue; fi
    
    # Wait for any background videos to finish before playing next
-   ( pkill -0 -P $$ mpv ) && wait $(pgrep -P $$ mpv)
-   
+   ( pkill -0 -P ${$} mpv ) && wait $(pgrep -P ${$} mpv)
+
+   # Try to track errors as best we can
+   if [[ ${?} -lt 3 ]]; then 
+      ((ERRCOUNT++))
+   elif [[ ${ERRCOUNT} -gt 0 ]]; then
+      ((ERRCOUNT--))
+   fi
+
    # Check if duration is longer than video and adjust accordingly
-   if [[ ${times[$vindex]} -lt ${dur} ]]; then
-      echo "Adjusting duration for shorter video length - ${times[$vindex]}s" 
-      oldur=${dur}
-      dur=${times[$vindex]}
-      starttime=$(($(rnum)%((${times[$vindex]}+1)-(${dur}))))
+   if [[ ${TIMES[${VINDEX}]} -lt ${DUR} ]]; then
+      echo "Adjusting duration for shorter video length - ${TIMES[${VINDEX}]}s" 
+      OLDUR=${DUR}
+      DUR=${TIMES[${VINDEX}]}
+      STARTTIME=$(($(rnum)%((${TIMES[${VINDEX}]}+1)-(${DUR}))))
    else
       # Set random start time based on duration
-      starttime=$(($(rnum)%((${times[$vindex]}+1)-${dur})))
+      STARTTIME=$(($(rnum)%((${TIMES[${VINDEX}]}+1)-${DUR})))
    fi
 
    # Start up video by index. Background so we can get the next video ready to play
-   echo "Video/Stream [${vindex}] - ${videos[$vindex]}"
-   echo "Title: ${titles[$vindex]}"
-   echo "Playing ${dur}s starting at ${starttime} out of ${times[$vindex]}s"
+   echo "Video/Stream [${VINDEX}] - ${VIDEOS[${VINDEX}]}"
+   echo "Title: ${TITLES[${VINDEX}]}"
+   echo "Playing ${DUR}s starting at ${STARTTIME} out of ${TIMES[${VINDEX}]}s"
    echo
-   playvid ${vindex}
+   playvid ${VINDEX}
 
    # Reset duration if it was changed for a short video
-   if [[ ${oldur} -gt 0 ]]; then
-      echo "Setting duration back to ${oldur}s"
+   if [[ ${OLDUR} -gt 0 ]]; then
+      echo "Setting duration back to ${OLDUR}s"
       echo
-      dur=${oldur}
-      unset oldur
+      DUR=${OLDUR}
+      unset -v OLDUR
    fi
 done
